94cc34c7c0ff2f21cbf2ecbac1f511194354bf57
Marat Dzhanybaev <maratkind@gmail.com>
2020-09-13 18:31:45 +1000

    Quick fix. Enable logging

--- TinkoffChat/Utils/Log.swift
+++ TinkoffChat/Utils/Log.swift
@@ -5,11 +5,12 @@
 //  Created by Marik on 12.09.2020.
 //  Copyright © 2020 Tinkoff. All rights reserved.
 //
-
+/// Utils class for logging
 public class Log {
+    /// Set false to disable logging
+    static var isEnabled: Bool = true
 
-    static var isEnabled: Bool = false
-
+    /// Print message to console
     static func d(_ message: String) {
         guard isEnabled else { return }
         print(message)
